e5a6d8fd757e87da5b3a049facc7c14e5de3d131
Liset Cayhualla Amaro <lise.cayhualla@tecsup.edu.pe>
2018-09-26 17:08:15 -0500

    commit2

--- ColeccionDeJuegos/ViewController.swift
+++ ColeccionDeJuegos/ViewController.swift
@@ -8,17 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSource{
+    
+    var juegos : [Juego] = []
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+      
+        
     }
+    
+    @IBAction func addbutton(_ sender: Any) {
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do{
+            try juegos = context.fetch(Juego.fetchRequest())
+            tableView.reloadData()
+        } catch{
+            
+        }
+    }
+    //funcion de la tabla para implementar las filas
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return juegos.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let juego = juegos[indexPath.row]
+        cell.textLabel?.text = juego.titulo
+        cell.imageView?.image = UIImage(data: (juego.imagen!) as Data)
+        return cell
+        
+    }
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
